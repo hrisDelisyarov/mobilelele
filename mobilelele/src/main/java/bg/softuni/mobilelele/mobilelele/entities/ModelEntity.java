7b875115058e5540bea3af0b804d8c29c6718473
Hris <h.delisiarov99@gmail.com>
2021-01-26 02:16:33 +0200

    Added Vehicle Model entity

---
+++ mobilelele/src/main/java/bg/softuni/mobilelele/mobilelele/entities/ModelEntity.java
@@ -0,0 +1,4 @@
+package bg.softuni.mobilelele.mobilelele.entities;
+
+public class ModelEntity {
+}
---
+++ mobilelele/src/main/java/bg/softuni/mobilelele/mobilelele/entities/enums/ModelCategoryEnum.java
@@ -0,0 +1,8 @@
+package bg.softuni.mobilelele.mobilelele.entities.enums;
+
+public enum ModelCategoryEnum {
+    CAR,
+    BUSS,
+    TRUCK,
+    MOTORCYCLE
+}
